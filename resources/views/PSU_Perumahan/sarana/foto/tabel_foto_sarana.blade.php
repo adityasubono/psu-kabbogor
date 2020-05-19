96d1a54fb0d18cc4aa70d0dbc3e9e74beb6400df
Aditya Subono <adityasubono@gmail.com>
2020-05-20 00:11:15 +0700

    Update

--- resources/views/PSU_Perumahan/sarana/foto/tabel_foto_sarana.blade.php
+++ resources/views/PSU_Perumahan/sarana/foto/tabel_foto_sarana.blade.php
@@ -1,3 +1,4 @@
+<link href="{!! asset('assets/css/perumahan.css') !!}" rel="stylesheet">
 <div class="card shadow mb-4">
     <div class="card-header py-3 bg-gray-500">
         <h6 class="m-0 font-weight-bold text-primary">Tabel Data Foto</h6>
