67f77588b36becac2dfd6082a0e0fdc5a8ca7344
Aditya Subono <adityasubono@gmail.com>
2020-05-29 03:08:29 +0700

    Update Monitoring

--- resources/views/PSU_Permukiman/monitoring/index.blade.php
+++ resources/views/PSU_Permukiman/monitoring/index.blade.php
@@ -12,6 +12,14 @@
         </div>
         <div class="card-body">
 
+            <video width="400" height="400" autoplay controls>
+                <source src="%StreamURL%" type="video/mp4">
+                <object width="320" height="240" type="application/x-shockwave-flash" data="http://releases.flowplayer.org/swf/flowplayer-3.2.5.swf">
+                    <param name="movie" value="http://releases.flowplayer.org/swf/flowplayer-3.2.5.swf" />
+                    <param name="flashvars" value='config={"clip": {"url": "%StreamURL%", "autoPlay":true, "autoBuffering":true}}' />
+                    <p><a href="%StreamURL%">view with external app</a></p>
+                </object>
+            </video>
 
         </div>
     </div>
