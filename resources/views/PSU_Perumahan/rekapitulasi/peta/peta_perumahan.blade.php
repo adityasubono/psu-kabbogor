b1413b18fad1cf97b36604a8bd630ebc73c0e2e6
Aditya Subono <adityasubono@gmail.com>
2020-05-31 04:10:45 +0700

    [adit] update polygon perumahan

--- resources/views/PSU_Perumahan/rekapitulasi/peta/peta_perumahan.blade.php
+++ resources/views/PSU_Perumahan/rekapitulasi/peta/peta_perumahan.blade.php
@@ -1,7 +1,5 @@
 <div id="mymap"></div>
-<script
-    src="http://maps.google.com/maps/api/js?key=AIzaSyBMbVQJuBRWDV1jFUVZ9Gzsu-nWOEr9LdM">
-</script>
+
 <script src="../../assets/js/gmap/gmaps.js"></script>
 <style type="text/css">
     #mymap {
@@ -27,15 +25,17 @@
 <script type="text/javascript">
     var locations = <?php print_r(json_encode($koordinat_perumahan)) ?>;
     var polygons={};
+    var infowindow={};
+    var bounds = {};
     var cords = {};
     var coord={};
     const perumahan_id = [];
-    var mymap = new GMaps({
-        el: '#mymap',
-        lat: -6.485213,
-        lng: 106.753537,
-        zoom: 12
-    });
+    // var mymap = new GMaps({
+    //     el: '#mymap',
+    //     lat: -6.485213,
+    //     lng: 106.753537,
+    //     zoom: 12
+    // });
     function onlyUnique(value, index, self) {
         return self.indexOf(value) === index;
     }
@@ -55,49 +55,103 @@
         for (var j=0; j < result[unique[u]].length; j++) {
             const lt = parseFloat(result[unique[u]][j].latitude);
             const ltd = parseFloat(result[unique[u]][j].longitude);
-            coord[j] = [lt,ltd];
+            coord[j] = {lat: lt, lng:ltd};
             cords[u].push(coord[j]);
-            polygons[u] = mymap.drawPolygon({
-                paths: cords[u],
-                strokeColor: '#000000',
-                strokeOpacity: 0.3,
-                strokeWeight: 2,
-                fillColor: '#00e676',
-                fillOpacity: 0.4
-            }, console.log('paths ini', polygons));
-            // $.each(locations, function (index, value) {
-            //     mymap.addMarker({
-            //         lat: value.latitude,
-            //         lng: value.longitude,
-            //         title: value.id,
-            //         infoWindow: {
-            //             content: '<div id="content">' +
-            //                 '<div id="siteNotice">' +
-            //                 '</div>' +
-            //                 '<h5 id="firstHeading" class="firstHeading">Perumahan Bumi Citra Asri</h5>'
-            //                 +
-            //                 '<div id="bodyContent">' +
-            //                 '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-            //                 'sandstone rock formation in the southern part of the ' +
-            //                 'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '
-            //                 +
-            //                 'south west of the nearest large town, Alice Springs; 450&#160;km ' +
-            //                 '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major ' +
-            //                 'features of the Uluru - Kata Tjuta National Park. Uluru is ' +
-            //                 'sacred to the Pitjantjatjara and Yankunytjatjara, the ' +
-            //                 'Aboriginal people of the area. It has many springs, waterholes, ' +
-            //                 'rock caves and ancient paintings. Uluru is listed as a World ' +
-            //                 'Heritage Site.</p>' +
-            //                 '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'
-            //                 +
-            //                 'https://en.wikipedia.org/w/index.php?title=Uluru</a> ' +
-            //                 '(last visited June 22, 2009).</p>' +
-            //                 '</div>' +
-            //                 '</div>',
-            //             maxWidth: 700
-            //         }
-            //     });
+            // polygons[u] = mymap.drawPolygon({
+            //     paths: cords[u],
+            //     strokeColor: '#000000',
+            //     strokeOpacity: 0.3,
+            //     strokeWeight: 2,
+            //     fillColor: '#00e676',
+            //     fillOpacity: 0.4,
+            //     infoWindow: {
+            //         content: '<h4>Eiffel Tower</h4><div>Paris, France</div>',
+            //         maxWidth: 100
+            //     }
             // });
         }
     }
+
+
+    function initMap() {
+        var map = new google.maps.Map(document.getElementById('mymap'), {
+            zoom: 12,
+            center: {
+                lat: -6.485213,
+                lng: 106.753537,
+            },
+        });
+        // Construction of polygon.
+        for (let u = 0; u < unique.length; u++) {
+            polygons[u] = new google.maps.Polygon({
+                paths: cords[u],
+                strokeColor: '#FF0000',
+                strokeOpacity: 0.6,
+                strokeWeight: 1,
+                fillColor: '#FF0000',
+                fillOpacity: 0.10
+            });
+            polygons[u].setMap(map);
+            bounds[u] = new google.maps.LatLngBounds();
+            for (var i = 0; i < polygons[u].getPath().getLength(); i++) {
+                bounds[u].extend(polygons[u].getPath().getAt(i));
+            }
+
+            infowindow[u] = new google.maps.InfoWindow();
+            infowindow[u].opened = false;
+
+            function mousefn(evt) {
+                infowindow[u].setContent("polygon<br>coords:" + bounds[u].getCenter().toUrlValue
+                (6));
+                infowindow[u].setPosition(bounds[u].getCenter());
+                infowindow[u].open(map);
+            }
+
+            google.maps.event.addListener(polygons[u], 'mouseover', mousefn);
+            // google.maps.event.addListener(mrpdPolygon, 'mousemove', mousefn);
+            google.maps.event.addListener(polygons[u], 'mouseout', function (evt) {
+                infowindow[u].close();
+                infowindow[u].opened = false;
+            });
+        }
+    }
+
+    // Definition of the LatLng coordinates for the polygon's path.
+    var polygonCoords = [{
+        lat: -16.4836,
+        lng: 145.4653
+    },
+        {
+            lat: -16.4500,
+            lng: 145.4133
+        },
+        {
+            lat: -16.2319,
+            lng: 145.4763
+        },
+        {
+            lat: -16.0878,
+            lng: 145.4548
+        },
+        {
+            lat: -16.0454,
+            lng: 145.9000
+        },
+        {
+            lat: -16.4861,
+            lng: 146.1269
+        },
+        {
+            lat: -16.7229,
+            lng: 145.6500
+        },
+        {
+            lat: -16.5913,
+            lng: 145.5192
+        },
+    ];
+</script>
+<script
+    src="
+    http://maps.google.com/maps/api/js?key=AIzaSyBMbVQJuBRWDV1jFUVZ9Gzsu-nWOEr9LdM&callback=initMap">
 </script>
