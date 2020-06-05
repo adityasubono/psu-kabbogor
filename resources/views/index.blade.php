a216c8b2dd5b1015d2458d034fe235b2754b78f0
Aditya Subono <adityasubono@gmail.com>
2020-06-05 11:28:47 +0700

    Update Login Form

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,30 +8,38 @@
     <!------ Include the above in your HEAD tag ---------->
 </head>
 <body id="LoginForm">
-<div class="sidenav">
-    <div class="login-main-text text-dark text-bold ">
 
-    </div>
-</div>
 <div class="main">
-    <h2 class="text-center mt-5">Sistem Informasi Prasana Sarana dan Utilitas
-        <br>Kabupaten Bogor</h2>
 
-    <div class="col-md-6 col-sm-12">
-        @if(\Session::has('alert'))
-        <div class="alert alert-danger">
-            <div>{{Session::get('alert')}}</div>
-        </div>
-        @endif
+    <div class="row">
+        <div class="col-sm-12">
+            @if(\Session::has('alert'))
+            <div class="alert alert-danger">
+                <div>{{Session::get('alert')}}</div>
+            </div>
+            @endif
 
-        @if(\Session::has('alert-success'))
-        <div class="alert alert-success">
-            <div>{{Session::get('alert-success')}}</div>
-        </div>
-        @endif
-        <div class="login-form">
+            @if(\Session::has('alert-success'))
+            <div class="alert alert-success">
+                <div>{{Session::get('alert-success')}}</div>
+            </div>
+            @endif
+
+            <div id="jam-digital">
+                <div id="hours">
+                    <p id="jam"></p>
+                </div>
+                <div id="minute">
+                    <p id="menit"></p>
+                </div>
+                <div id="second">
+                    <p id="detik"></p>
+                </div>
+            </div>
+            <div class="login-form">
             <form method="post" action="/loginpost">
                 {{ csrf_field() }}
+
 
                 <div class="form-group">
                     <h3 class="border_h1">Login Sistem</h3>
@@ -42,12 +50,14 @@
                 </div>
                 <div class="form-group">
                     <label for="password">Password</label>
-                    <input type="password"  id="password" class="form-control" name="password"
+                    <input type="password" id="password" class="form-control" name="password"
                            placeholder="Masukan Password">
                 </div>
 
                 <button type="submit" class="btn btn-black">Login</button>
             </form>
+            </div>
+
         </div>
     </div>
 </div>
@@ -61,4 +71,15 @@
             $(this).remove();
         });
     }, 4000);
+</script>
+
+<script type="text/javascript">
+    window.setTimeout("waktu()",1000);
+    function waktu() {
+        var tanggal = new Date();
+        setTimeout("waktu()",1000);
+        document.getElementById("jam").innerHTML = tanggal.getHours();
+        document.getElementById("menit").innerHTML = tanggal.getMinutes();
+        document.getElementById("detik").innerHTML = tanggal.getSeconds();
+    }
 </script>
